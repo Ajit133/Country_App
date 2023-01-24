3ba20706fe9d7c86dc3fd22e137b895708226059
Ajit133 <dasajit697@gmail.com>
2023-01-24 07:15:42 +0600

    Add Remove Button

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ const App = () =>{
        const[isLoading,setIsLoading] = useState(true)
        const[error,setError] = useState(null)
        const[countries,setCountries] = useState([]);
+       const[filteredCountries,setFilteredCountries] = useState(countries);
+       
        const fetchData = async(url)=>{
         setIsLoading(true);
         try{
           const response = await fetch(url);
           const data = await response.json();
+          setFilteredCountries(data)
           setCountries(data);
+
           setIsLoading(false);
           setError(null);
           
@@ -25,16 +29,19 @@ const App = () =>{
       // console.log(user)
    
        const RemoveItems = (name)=>{
-           alert(name)
+           const filter = filteredCountries.filter((country)=>
+           country.name.common !== name);
+           setFilteredCountries(filter);
        }
 
        useEffect(()=>{
           fetchData(url)
        },[])
+
        return(<>
              {isLoading && <h2>Loading....</h2>}
              {error && <h2>{error.message}</h2>}
-             {countries && <Countries RemoveItems={RemoveItems} countries={countries} />}
+             {countries && <Countries RemoveItems={RemoveItems} countries={filteredCountries} />}
              </>
        )
 }
